bf4b2670cb934e565be0bc3616d41f090154b8d7
Pugsy <nimsarakarunaratne@gmail.com>
2026-01-15 13:28:15 +0530

    Checked exceptions are exceptions that are declared at compile time and therefore must be either cuaght using a try-catch block or declared using _throws_

---
+++ src/CheckedVsUnchecked.java
@@ -0,0 +1,19 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class CheckedVsUnchecked  {
+
+    public static void main(String[] args) {
+        readFile("myFile.txt");
+    }
+
+    private static void readFile(String fileName){
+        try{
+            FileReader reader = new FileReader(fileName);
+        }
+        catch(FileNotFoundException fnfe){
+            System.out.println("Hey, that file does not exist");
+        }
+    }
+}
+
--- src/Exceptions.java
+++ src/Exceptions.java
@@ -10,7 +10,7 @@ public class Exceptions {
             result = x / y; // Line B
             System.out.println("Result: " + result);
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("CATCH 1: " + e.getMessage());
+            System.out.println("CATCH 1 : " + e.getMessage());
         } catch (ArithmeticException e) {
             System.out.println("CATCH 2: " + e.getMessage());
         } catch (Exception e) {
